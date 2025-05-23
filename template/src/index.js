35a436c3c06bb263b7b62232c9f29755fceb6e41
Linzp <sunandmoon001@163.com>
2025-05-23 17:52:50 +0800

    天际啊国际化配置

--- template/src/index.js
+++ template/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import zhCn from './locale/zh-CN';
+import enUS from './locale/en-US';
+import { createWithIntlProvider, useIntl, localeLoader } from '@kne/react-intl';
 import style from './style.module.scss';
 
-const <%=templateLibs.camelCase(name)%> = ()=>{
+localeLoader('en-US', enUS, <%=name%>);
+
+const <%=templateLibs.camelCase(name)%> = createWithIntlProvider('zh-CN',
+  zhCn,
+  <%=name%>)(()=>{
+    const { formatMessage } = useIntl();
     return <span className={style['tips']}>我是一个初始化组件</span>
-};
+});
 
 export default <%=templateLibs.camelCase(name)%>;
